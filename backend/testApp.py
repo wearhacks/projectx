186250d2cab43beb84b2a797cb2133445b8f70db
shehaaz <shehaaz@gmail.com>
2015-02-08 19:28:49 -0800

    tests

---
+++ backend/testApp.py
@@ -0,0 +1,6 @@
+import os
+from flask import Flask, request, render_template, url_for, redirect, session, g
+
+payload = {'key1': 'value1', 'key2': 'value2'}
+
+r = requests.post("http://127.0.0.1:5000/getUser/1", data=payload)
