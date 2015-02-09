74b6e3848a67bab8c47bee3b34f429fb773a921b
shehaaz <shehaaz@gmail.com>
2015-02-08 17:57:24 -0800

    started firebase codin'

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,37 @@
+import os
 from flask import Flask, request, render_template, url_for, redirect, session, g
-from cassandra.cluster import Cluster
+from firebase import firebase
+
 
 app = Flask(__name__)
 
-cluster = Cluster()
-cass = cluster.connect()
+firebase = firebase.FirebaseApplication(os.environ['DB'], None)
+
+result = firebase.get('/hello', None)
+print result
+
+data = {'name': 'Ozgur Vatansever'}
+
+firebase.post('/users/1', data)
+
 
 @app.route('/')
 def index():
 	return "hello world"
+
+@app.route('/getUser/<id>')
+def getUser(id):
+	return firebase.get('/users/'+id)
+
+@app.route('/addUser/<id>', methods = ['POST'])
+def addUser(id):
+
+    if request.headers['Content-Type'] == 'application/json':
+    	data = json.dumps(request.json)
+    	firebase.post('/users/'+id, data)
+        return "JSON Message: " + data
+    else:
+        return "415 Unsupported Media Type ;)"		
 
 
 if __name__ == '__main__':
