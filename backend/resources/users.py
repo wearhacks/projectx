26a9c04c2ce4a256ecdec80897f04f93038f98bb
ueg1990 <uehtesham90@gmail.com>
2015-02-10 00:48:11 -0500

    Make request.json work

--- backend/resources/users.py
+++ backend/resources/users.py
@@ -1,36 +1,41 @@
+from flask import request
 from flask.ext import restful
 from flask.ext.restful import fields, marshal_with, reqparse
 from firebase import firebase
 import os
+import json
 
 fb = firebase.FirebaseApplication(os.environ['DB'], None)
 
-parser = reqparse.RequestParser()
-parser.add_argument(
-    'username', dest='username',
-    type=str , location='json',
-    required=True, help='The user\'s username',
-)
-parser.add_argument(
-    'email', dest='email',
-    type=str, location='json',
-    required=True, help='The user\'s email',
-)
+# parser = reqparse.RequestParser()
+# parser.add_argument(
+#     'username', dest='username',
+#     type=str , location='json',
+#     required=True, help='The user\'s username',
+# )
+# parser.add_argument(
+#     'email', dest='email',
+#     type=str, location='json',
+#     required=True, help='The user\'s email',
+# )
 
-user_fields = {
-    'username': fields.String,
-    'email': fields.String,
-}
+# user_fields = {
+#     'username': fields.String,
+#     'email': fields.String,
+# }
 
 class Users(restful.Resource):
 
-	@marshal_with(user_fields)
+	# @marshal_with(user_fields)
 	def get(self, _id):
 	    # args = post_parser.parse_args()
 	    return fb.get('/users/'+_id, None)
 
-	@marshal_with(user_fields)
+	# @marshal_with(user_fields)
 	def post(self, _id):
-		args = parser.parse_args()
-		data = {'username' : args.username, 'email' : args.email}
+		# args = parser.parse_args()
+		# data = {'username' : args.username, 'email' : args.email}
+		print "data", request.json
+		data = json.dumps(request.json)
 		fb.post('/users/'+_id, data)
+		return data
